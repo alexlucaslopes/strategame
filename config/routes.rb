ca9f0d879ec01746ab6ecd37491b2da4f2ae72ca
Alex <Alex@Kaisas-MacBook-Air.local>
2017-06-04 12:42:03 -0700

    finished devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  devise_for :users
   resources :comments
   resources :games
   resources :users
+  root 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
